5e74e16386aa508c866aaba79dd098cc985a7cdc
egilp04 <egilp04@iesalbarregas.es>
2025-06-02 11:49:24 +0200

    cambios js

--- src/pages/forum/forum.js
+++ src/pages/forum/forum.js
@@ -5,7 +5,9 @@ let personIcon = document.querySelector('.personIcon');
 //Propio
 let titleForum = document.querySelector('.titleForum');
 let categoryforum = document.querySelector('.categoryForum');
-let likeCounter = 0;
+let forumIcon = document.getElementById('favoriteForumIcon');
+let resourceList = document.getElementById('resourceList');
+let data = null;
 // const forumElementos = ...
 
 
@@ -16,24 +18,45 @@ async function getForumData() {
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
-    const data = await response.json();
+    data = await response.json();
     return data;
   } catch (error) {
     console.error('There has been a problem with your fetch operation:', error);
   }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Función para mostrar los datos en el foro
-    
+//Función para cambiar la clase de la lista de recursos, según si tiene o no elementos hijos
+function seeResources() {
+    if (resourceList && resourceList.children.length === 0) {
+        resourceList.classList.add('resourceListEmpty');
+    } else if (resourceList) {
+        resourceList.classList.remove('resourceListEmpty');
+    }
+}
 
-    //Rellenar enlaces con sus iconos 
+function fillData(){
+  titleForum.textContent =  data.title;;
+  categoryforum.textContent = data.category;
+  //nombre del usuaerio
+}
 
+function giveLike(){
 
-    //Funcion para dar y contar los likes a los recursos 
+}
 
-   
+//Funciones para que se ejecuten después de cargar el DOM
+document.addEventListener('DOMContentLoaded', async function() {
+  data = await getForumData();
+  seeResources();
+ const observer = new MutationObserver(seeResources);
+  if (resourceList) {
+    observer.observe(resourceList, { childList: true });
+  }
   
+  data = await getForumData();  
+
+  if (data) fillData();
+  else console.log('No data found for the forum');
 });
 
 

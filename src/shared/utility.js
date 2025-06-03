0a040e0954a6ce008f8a41e6d60ca51e25ab97c8
egilp04 <egilp04@iesalbarregas.es>
2025-06-03 10:53:43 +0200

    cambios

--- src/shared/utility.js
+++ src/shared/utility.js
@@ -2,8 +2,8 @@ let user = document.getElementById('user');
 
 //funcion para rellenar el nombre de usuario según el email acortado
 function fillMainUser(){
-const userEmail = sessionStorage.getItem('userEmail');
-user.textContent = userEmail ? userEmail.split('@')[0] : 'Usuario';
+const userInfo = JSON.parse(sessionStorage.getItem('user'));
+user.textContent = userInfo.email ? userInfo.email.split('@')[0] : 'Usuario';
 }
 
 //Función para guardar y recuperar la página anterior
@@ -23,8 +23,8 @@ function goBack() {
     window.location.href = '/src/pages/index/index.html';
   }
 }
-//Añadir la llamada a la funcion:   setPreviousPage('.go-to-forum'); y la classe a todos los elementos que lleven al foro
 
+//Añadir la llamada a la funcion:   setPreviousPage('.go-to-forum'); y la classe a todos los elementos que lleven al foro
 function setPreviousPageOnClick(selector) {
   document.querySelector(selector).addEventListener('click', function() {
       sessionStorage.setItem('previousPage', window.location.pathname);
